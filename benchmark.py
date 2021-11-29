17f32390db7724722d9cfeef589242687e810125
Thomas <thomas27johnson39@gmail.com>
2021-11-29 16:08:03 +0000

    Add compare function and change bench output

--- benchmark.py
+++ benchmark.py
@@ -20,10 +20,37 @@ def bench(func, iterations=1, *args):
     # Call func n times.
     for repeat in range(iterations):
         func(*args)
-    totalTime += time.time() - start
+    totalTime = time.time() - start
 
     # Output.
-    output = f"Total time taken: {totalTime:.3f}s\n" + f"Average time: {totalTime/iterations:.3f}s\n"
+    output = f"Function: {func.__name__}:\nTotal time taken: {totalTime:.3f}s\n" + f"Average time: {totalTime/iterations:.3f}s\n"
     print(output, end="")
-    
     return totalTime
+
+def compare(func1, func2, iterations=1, *args):
+    """Allows you to compare to similar functions
+    Both functions will be given the same arguments so they mnust be similar
+    in structure.
+
+    Runs each function based on the number of iterations given. Displays how much faster
+    a function was as a percentage.
+
+    Returns: A float of the total difference in time between the execution of the two
+    functions."""
+    # No need to type check or value check because it is done inside bench().
+    time_func1 = bench(func1, iterations, *args)
+    time_func2 = bench(func2, iterations, *args)
+
+    sep = "------------------------------\n"
+    
+    # Find out which function was faster.
+    if time_func1 < time_func2:
+        percentage = (time_func2 / time_func1) * 100
+        print(sep + f"{func1.__name__}: +{percentage:.2f}%\n", end = "")
+    elif time_func1 > time_func2:
+        percentage = (time_func1 / time_func2) * 100
+        print(sep + f"{func2.__name__}: +{percentage:.2f}%\n", end = "")
+    else: # Equal execution time.
+        print(f"{func1.__name__} == {func2.__name__}")
+    
+    
